d604ae2750fdc2aff9c96b6f9ff8a935b78fdc6e
codebot-robot <codebot-robot@google.com>
2026-07-02 22:14:08 +0000

    opentelemetry: copy module from prototype branch
    Copy the opentelemetry directory and module from the prototype branch so we can continue the work on the main branch after the repo reorganization.
    
    Fixes #136

---
+++ opentelemetry/cmd/opentelemetry-sink/pb/file.go
@@ -0,0 +1,87 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pb
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type ObjectType struct {
+	TypeCode uint32
+	TypeName string
+}
+
+func (x *ObjectType) Marshal() []byte {
+	// Tag 1 (type_code): 0x08 (field 1, type varint)
+	// Tag 2 (type_name): 0x12 (field 2, type length-delimited)
+	var buf bytes.Buffer
+
+	// Write type_code
+	buf.WriteByte(0x08)
+	var tmp [10]byte
+	n := binary.PutUvarint(tmp[:], uint64(x.TypeCode))
+	buf.Write(tmp[:n])
+
+	// Write type_name
+	buf.WriteByte(0x12)
+	n = binary.PutUvarint(tmp[:], uint64(len(x.TypeName)))
+	buf.Write(tmp[:n])
+	buf.WriteString(x.TypeName)
+
+	return buf.Bytes()
+}
+
+func (x *ObjectType) Unmarshal(data []byte) error {
+	var i int
+	for i < len(data) {
+		tag, n := binary.Uvarint(data[i:])
+		if n <= 0 {
+			break
+		}
+		i += n
+
+		fieldNum := tag >> 3
+		wireType := tag & 7
+
+		switch fieldNum {
+		case 1:
+			if wireType != 0 {
+				return nil // Ignore errors for simplicity
+			}
+			val, n := binary.Uvarint(data[i:])
+			if n <= 0 {
+				break
+			}
+			i += n
+			x.TypeCode = uint32(val)
+		case 2:
+			if wireType != 2 {
+				return nil
+			}
+			length, n := binary.Uvarint(data[i:])
+			if n <= 0 {
+				break
+			}
+			i += n
+			x.TypeName = string(data[i : i+int(length)])
+			i += int(length)
+		default:
+			// Skip for simplicity assuming we only have these 2
+			return nil
+		}
+	}
+	return nil
+}
